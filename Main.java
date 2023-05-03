d7460bdbbeac8c365e75a7904cd31e5e7d263c85
Juan Manuel Quijano Pardo <110079353+Juanquijanop@users.noreply.github.com>
2023-05-03 11:15:13 -0500

    Simulating Db using Map Data

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> problemasSoluciones = new HashMap<>(); // Mapa para almacenar los problemas y soluciones
+        
+    }
+}
