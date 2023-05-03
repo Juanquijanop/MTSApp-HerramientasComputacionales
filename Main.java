5f6381b81c9de144dd9bd5aa3f4652ab54977529
Juan Manuel Quijano Pardo <110079353+Juanquijanop@users.noreply.github.com>
2023-05-03 11:17:49 -0500

    Creating firts Validation of the do While

--- Main.java
+++ Main.java
@@ -8,7 +8,17 @@ public class Main {
         
         problemasSoluciones.put("0000", "Ejemplo"); // Podemos agregar aqui los datos de la BD
 
+        Scanner scanner = new Scanner(System.in);
+        String codigoProblema;
         
+        // Bucle que se ejecuta hasta que el usuario ingresa "0" para salir
+        do {
+            System.out.println("Ingrese el código del problema (0 para salir): ");
+            codigoProblema = scanner.nextLine();
+            
+        } while (!codigoProblema.equals("0"));
+        
+        System.out.println("¡Gracias por utilizar nuestro sistema de solución de problemas de maquinaria!");
         
     }
 }
