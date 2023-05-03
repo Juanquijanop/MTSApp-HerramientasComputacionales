4854c80b59140b325a3f41c9150f9b55c0d05b4b
Juan Manuel Quijano Pardo <110079353+Juanquijanop@users.noreply.github.com>
2023-05-03 11:16:06 -0500

    Adding Some examples in the DB

--- Main.java
+++ Main.java
@@ -6,5 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Map<String, String> problemasSoluciones = new HashMap<>(); // Mapa para almacenar los problemas y soluciones
         
+        problemasSoluciones.put("0000", "Ejemplo"); // Podemos agregar aqui los datos de la BD
+
+        
+        
     }
 }
