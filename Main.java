d5789bd2dfeeaed284afe294dcecc1f48208c5ad
Juan Manuel Quijano Pardo <110079353+Juanquijanop@users.noreply.github.com>
2023-06-02 20:58:59 -0500

    Adding more possible warnings on machine

--- Main.java
+++ Main.java
@@ -23,6 +23,26 @@ public class Main {
                 "Solución: Paso 2 - Equilibre o reemplace los componentes desgastados\n" +
                 "Solución: Paso 3 - Ajuste los soportes y bases de la máquina para reducir las vibraciones");
 
+        problemasSoluciones.put("E4", "Error: Pérdida de conexión de red\n" +
+                "Solución: Paso 1 - Verifique que los cables de red estén correctamente conectados\n" +
+                "Solución: Paso 2 - Reinicie el enrutador o el dispositivo de red\n" +
+                "Solución: Paso 3 - Compruebe la configuración de red en el dispositivo y realice las correcciones necesarias");
+
+        problemasSoluciones.put("E5", "Error: Pantalla en blanco\n" +
+                "Solución: Paso 1 - Verifique que el cable de video esté correctamente conectado\n" +
+                "Solución: Paso 2 - Reinicie el monitor o la pantalla\n" +
+                "Solución: Paso 3 - Compruebe la tarjeta gráfica y actualice los controladores si es necesario");
+
+        problemasSoluciones.put("E6", "Error: Batería agotada\n" +
+                "Solución: Paso 1 - Conecte el dispositivo a una fuente de alimentación\n" +
+                "Solución: Paso 2 - Espere a que la batería se cargue por completo\n" +
+                "Solución: Paso 3 - Reemplace la batería si sigue descargándose rápidamente");
+
+        problemasSoluciones.put("E7", "Error: Archivo corrupto\n" +
+                "Solución: Paso 1 - Intente abrir el archivo en otro programa o dispositivo\n" +
+                "Solución: Paso 2 - Restaure una copia de seguridad anterior del archivo\n" +
+                "Solución: Paso 3 - Utilice herramientas de reparación de archivos para intentar recuperar los datos");
+
         Scanner scanner = new Scanner(System.in);
         String codigoProblema;
 
